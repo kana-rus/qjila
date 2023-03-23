4a729ae33fd30a409222a9b72bfee6295ef57060
kana-rus <kanarus786@gmail.com>
2023-03-23 22:32:52 +0900

    save@2023-03-23 22:32+9:00

--- src/operation/count.rs
+++ src/operation/count.rs
@@ -23,26 +23,6 @@ impl<E: Entity> Count<E> {
     }
 }
 const _: (/* Count impls */) = {
-    // impl<E: Entity> IntoFuture for Count<E> {
-    //     type Output = Result<usize, crate::error::Error>;
-    //     type IntoFuture = Pin<Box<dyn Future<Output = Self::Output>>>;
-    //     fn into_future(self) -> Self::IntoFuture {
-    //         Box::pin(async move {
-    //             let client = &self.connection.0;
-    //             let stmt = client
-    //                 .prepare_cached(&format!(
-    //                     "SELECT COUNT(*) FROM {}",
-    //                     E::TABLE_NAME,
-    //                 ))
-    //                 .await?;
-    //             let row = client
-    //                 .query_one(&stmt, &[])
-    //                 .await?;
-    //             Ok(row.get::<_, i64>(0) as usize)
-    //         })
-    //     }
-    // }
-
     impl<E: Entity> IntoFuture for Count<E> {
         type Output = Result<usize, Error>;
         type IntoFuture = CountResult;
@@ -64,9 +44,18 @@ const _: (/* Count impls */) = {
     } impl Future for CountResult {
         type Output = Result<usize, Error>;
         fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-            
-
-            todo!()
+            let client = &self.connection.0;
+            match pin!(client.prepare_cached(&self.sql)).poll(cx) {
+                Poll::Pending         => return Poll::Pending,
+                Poll::Ready(Err(e))   => return Poll::Ready(Err(e.into())),
+                Poll::Ready(Ok(stmt)) => {cx.waker().wake_by_ref();
+                    match pin!(client.query_one(&stmt, &[])).poll(cx) {
+                        Poll::Pending        => Poll::Pending,
+                        Poll::Ready(Err(e))  => Poll::Ready(Err(e.into())),
+                        Poll::Ready(Ok(row)) => Poll::Ready(Ok(row.get::<_, i64>(0) as usize)),
+                    }
+                },
+            }
         }
     }
 };
