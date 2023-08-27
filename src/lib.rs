4278611993fb46472454bad9d9380c324623362b
kana-rus <kanarus786@gmail.com>
2023-08-28 04:58:22 +0900

    save@2023-08-28 04:58+9:00

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-const C: usize = 42;
+const fn c() -> usize {42}
 
-struct T {
-    a: C
+const fn f(i: usize) -> usize {
+    i.pow(10).rem_euclid(31)
+}
+
+struct T1<const C: usize = 42>;
+struct T2<const C: usize = 42>;
+struct T3<const C: usize = 42>;
+
+struct Table {
+    id: T1::<{c()}>,
 }
