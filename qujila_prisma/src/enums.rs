a7fe7781a716b3f380dd670420a162d49f632d7a
kana-rus <kanarus786@gmail.com>
2023-09-07 20:17:42 +0900

    Parse for Enum

--- qujila_prisma/src/enums.rs
+++ qujila_prisma/src/enums.rs
@@ -1,4 +1,23 @@
+use crate::*;
+
 pub struct Enum {
     pub name:     String,
     pub variants: Vec<String>,
+}
+
+impl Parse for Enum {
+    fn parse(ts: &mut TokenStream) -> Result<Self, std::borrow::Cow<'static, str>> {
+        ts.try_consume(Token::Keyword(Keyword::_enum))?;
+
+        let name = ts.try_pop_ident()?.name;
+
+        ts.try_consume(Token::BraceOpen)?;
+        let mut variants = Vec::new();
+        while let Ok(v) = ts.try_pop_ident() {
+            variants.push(v.name)
+        }
+        ts.try_consume(Token::BraceClose)?;
+
+        Ok(Self { name, variants })
+    }
 }
