f915243b5f98452c6fcb975fd18bb418140444e7
kana-rus <kanarus786@gmail.com>
2023-09-17 17:48:40 +0900

    TODO: generate_orm

--- qujila_orm/src/generater/mod.rs
+++ qujila_orm/src/generater/mod.rs
@@ -1,10 +1,17 @@
-use std::{path::Path, borrow::Cow};
-
 use qujila_prisma::db_type::Model;
+
+use std::{
+    format as f,
+    fs,
+    path::Path,
+    borrow::Cow,
+};
 
 
 pub fn generate_orm(model: Model, output_dir: &Path) -> Result<(), Cow<'static, str>> {
-    
+    let mut output_file = fs::File::create(
+        output_dir.join(f!("{}.rs", (&*model.name).to_lowercase()))
+    ).map_err(|e| Cow::Owned(f!("Failed to open file: {e}")))?;
 
     todo!()
 }
